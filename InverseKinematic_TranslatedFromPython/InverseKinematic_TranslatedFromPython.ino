fc7b32e95b90708ac86d0dee420321424ad464d5
teknikelektromdp <103289258+teknikelektromdp@users.noreply.github.com>
2023-04-10 16:15:33 +0700

    Update InverseKinematic_TranslatedFromPython.ino

--- InverseKinematic_TranslatedFromPython/InverseKinematic_TranslatedFromPython.ino
+++ InverseKinematic_TranslatedFromPython/InverseKinematic_TranslatedFromPython.ino
@@ -1,11 +1,8 @@
-double coxa=3,     femur=7,      tibia=9;
+double coxa=6.5,     femur=4.7,      tibia=6;
 
-double x_limit=5,  y_limit=8.66, z_limit=3;
+double x_limit=11.5,  y_limit=11.5, z_limit=5;
 
-double x_val=5,    y_val=7,      z_val=3;
-
-double beta_limit=56,      alpha_limit=143;
-
+double x_val=11.5,    y_val=11.5,      z_val=4;
 
 double Rad_to_Degree(double Rad){
   double degree= Rad*57.2957795;
@@ -23,19 +20,19 @@ double Rad_to_Degree(double Rad){
 //  alpha_total=alpha1+alpha2;
 //  return gamma, beta, alpha_total;
 //}
-int Gamma_Calculation(double x, double y, double z){
+double Gamma_Calculation(double x, double y, double z){
   double gamma;
   gamma= Rad_to_Degree(atan(x/y));
   return gamma;
 }
-int Beta_Calculation(double x, double y, double z){
+double Beta_Calculation(double x, double y, double z){
   double L1, beta, L;
   L1=sqrt(pow(x, 2) + pow(y, 2));
   L= sqrt(pow((L1-coxa), 2) + pow(z, 2));
   beta= Rad_to_Degree(acos((pow(tibia, 2) + pow(femur, 2) - pow(L, 2))/(2*tibia*femur)));
   return beta;
 }
-int TotalAlpha_Calculation(double x, double y, double z){
+double TotalAlpha_Calculation(double x, double y, double z){
   double L1, L, alpha1, alpha2, alpha_total;
   L1=sqrt(pow(x, 2) + pow(y, 2));
   L= sqrt(pow((L1-coxa), 2) + pow(z, 2));
