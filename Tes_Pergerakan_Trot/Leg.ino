e1aa8dc3d80507cdb539b7261714d2ba739c826f
teknikelektromdp <103289258+teknikelektromdp@users.noreply.github.com>
2023-04-16 11:34:29 +0700

    Update Leg.ino

--- Tes_Pergerakan_Trot/Leg.ino
+++ Tes_Pergerakan_Trot/Leg.ino
@@ -6,13 +6,6 @@ void LeftFront(double x_val, double y_val, double z_val){
   delay(servo_delay);
   Dynamixel.moveSpeed(9, dynamixelGamma(x_val, y_val, z_val, 0, 0) ,servo_speed);
   delay(servo_delay);
-//from python simulation
-//  Dynamixel.moveSpeed(22, dynamixelBeta(x_val, y_val, z_val, 1, 0) ,servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(20, dynamixelAlpha(x_val, y_val, z_val, 0, 0) ,servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(9, dynamixelGamma(x_val, y_val, z_val, 1, 0) ,servo_speed);
-//  delay(servo_delay);
 }
 
 void LeftBack(double x_val, double y_val, double z_val){
@@ -23,13 +16,6 @@ void LeftBack(double x_val, double y_val, double z_val){
   delay(servo_delay);
   Dynamixel.moveSpeed(12, dynamixelGamma(x_val, y_val, z_val, 0, 0) ,servo_speed);
   delay(servo_delay);
-  //from python simulation
-//  Dynamixel.moveSpeed(10, dynamixelBeta(x_val, y_val, z_val, 1, 0) ,servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(17, dynamixelAlpha(x_val, y_val, z_val, 0, 0) ,servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(12, dynamixelGamma(x_val, y_val, z_val, 0, 0) ,servo_speed);
-//  delay(servo_delay);
 }
 
 void RightFront(double x_val, double y_val, double z_val){
@@ -40,13 +26,6 @@ void RightFront(double x_val, double y_val, double z_val){
   delay(servo_delay);
   Dynamixel.moveSpeed(2, dynamixelGamma(x_val, y_val, z_val, 1, 0) ,servo_speed);
   delay(servo_delay);
-  //from python simulation
-//  Dynamixel.moveSpeed(14, dynamixelBeta(x_val, y_val, z_val, 1, 1) ,servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(5, dynamixelAlpha(x_val, y_val, z_val, 0, 1) ,servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(2, dynamixelGamma(x_val, y_val, z_val, 0, 0) ,servo_speed);
-//  delay(servo_delay);
 }
 void RightBack(double x_val, double y_val, double z_val){
   //Right-Back
@@ -56,11 +35,4 @@ void RightBack(double x_val, double y_val, double z_val){
   delay(servo_delay);
   Dynamixel.moveSpeed(1, dynamixelGamma(x_val, y_val, z_val, 1, 0), servo_speed);
   delay(servo_delay);
-  //from python simulation
-//  Dynamixel.moveSpeed(11, dynamixelBeta(x_val, y_val, z_val, 1, 1), servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(18, dynamixelAlpha(x_val, y_val, z_val, 0, 1), servo_speed);
-//  delay(servo_delay);
-//  Dynamixel.moveSpeed(1, dynamixelGamma(x_val, y_val, z_val, 1, 0), servo_speed);
-//  delay(servo_delay);
 }
